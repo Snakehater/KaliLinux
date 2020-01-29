90bc6f17f165874069bb3e8f8ff0f67cc3320954
Snakehater <vigor.turujlija@gmail.com>
2020-01-30 10:44:43 +1100

    Jan 30 00:43, 2020

---
+++ flag_ip.py
@@ -0,0 +1,23 @@
+import socket
+hostname = socket.gethostname()
+#IpAddr = socket.gethostbyname(hostname)
+
+import fcntl
+import struct
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+
+IpAddr = get_ip_address('wlan0')
+print 'Ip address is: ' + IpAddr
+
+import requests
+url = 'https://elvigo.com/vigor/API/RpiIp.php?ip='+IpAddr+'&hostname='+hostname
+
+requests.get(url)
